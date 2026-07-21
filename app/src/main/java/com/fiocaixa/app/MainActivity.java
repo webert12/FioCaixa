640fd0d3ab185cdd8512dec2e9e7e34f6be4ce74
webert12 <56725537+webert12@users.noreply.github.com>
2026-07-20 23:09:10 -0300

    Add MainActivity with WebView implementation

---
+++ app/src/main/java/com/fiocaixa/app/MainActivity.java
@@ -0,0 +1,26 @@
+package com.fiocaixa.app;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        WebView webView = new WebView(this);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        
+        webView.setWebViewClient(new WebViewClient());
+        
+        // SUBSTITUA O LINK ABAIXO PELO LINK DO SEU SISTEMA (Streamlit / Web)
+        webView.loadUrl("https://github.com/webert12/FioCaixa");
+        
+        setContentView(webView);
+    }
+}
